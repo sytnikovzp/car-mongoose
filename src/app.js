dcc990c5d227b8daea46810855773126e5b2b4b4
alex <sytnikov.zp@gmail.com>
2024-08-30 14:16:50 +0300

    Added middlewares to app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 // ================================
+const {
+  errorHandlers: { validationErrorHandler, errorHandler },
+} = require('./middlewares');
+
+const {
+  time: { getTime, showTime },
+} = require('./middlewares');
+// ============================
 const router = require('./routers');
 
 const app = express();
 
+app.use(
+  cors({
+    exposedHeaders: ['X-Total-Count'],
+  })
+);
+
 app.use(express.json());
 
+app.use(getTime, showTime);
+
+// ============================
+//  Car shop APP
+// ============================
 app.use('/api', router);
+app.use(validationErrorHandler, errorHandler);
 
 module.exports = app;
