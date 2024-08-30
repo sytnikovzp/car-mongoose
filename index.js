6ad892985d84bc611db91372b802095b4d9e4f6a
alex <sytnikov.zp@gmail.com>
2024-08-30 17:54:49 +0300

    Added 'createServer' from 'http'

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path');
 // ================================
 require('dotenv').config();
 const mongoose = require('mongoose');
+const { createServer } = require('http');
 // ================================
 const app = require('./src/app');
 const dbMongo = require('./src/models');
@@ -9,8 +10,9 @@ const { cars, types } = require('./src/constants');
 // ==========================
 const env = process.env.NODE_ENV || 'development';
 const pathToConfig = path.resolve('src', 'config', 'mongoConfig');
-
 const config = require(pathToConfig)[env];
+
+const server = createServer(app);
 
 mongoose
   .connect(`mongodb://${config.host}:${config.port}/${config.dbName}`)
@@ -35,6 +37,9 @@ const createCars = async () => {
 
 // createCars()
 
+const HOST = process.env.HOST;
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`));
+server.listen(PORT, HOST, () => {
+  console.log(`Server has been started on 'http://${HOST}:${PORT}'.`);
+});
